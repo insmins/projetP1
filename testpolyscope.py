040b52da44b73f02efffd01931628a1381212267
Inès El Hadri <ines.el.hadri@etu.imt-nord-europe.fr>
2024-09-12 15:26:15 +0200

    test polyscope + valeurs enregistrées

--- testpolyscope.py
+++ testpolyscope.py
@@ -2,6 +2,7 @@ import polyscope as ps
 from Camera import Camera
 import numpy as np
 import cv2
+from filter_outliers import up_down_limits
 
 ps.init()
 
@@ -11,15 +12,24 @@ frames, aligned_frames, aligned_depth_frame, color_frame = cam.updateCam()
 
 depth_image = np.asanyarray(aligned_depth_frame.get_data())
 
-# cv2.imshow("Image", depth_image)
+# retirer les valeurs aberrantes
+flat_depth = depth_image.flatten()
+# flat_depth = removeOutliers(flat_depth, 2)
+borne_inf, borne_sup = up_down_limits(flat_depth, 2)
+mediane = np.median(flat_depth)
 
+# valeurs aberrantes = mediane et creation de la liste des points x, y, z
 xyz = []
 for x in range(depth_image.shape[0]):
     for y in range(depth_image.shape[1]):
-        xyz.append([x, y, depth_image[x, y]])
+        if depth_image[x, y] <=borne_inf or depth_image[x, y]>= borne_sup: #si en dehors des bornes alors valeur aberrante
+            depth_image[x, y] = mediane
+        xyz.append([x, y, depth_image[x, y]])        
 
 xyz = np.asanyarray(xyz)
-print(xyz)
+
+np.savetxt("xyz_traite.txt", xyz)
+
 
 
 # visualize!
