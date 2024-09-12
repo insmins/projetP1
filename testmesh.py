4bca8dee55939fea7dd2346ea86d8edd282a7c67
Inès El Hadri <ines.el.hadri@etu.imt-nord-europe.fr>
2024-09-12 11:34:58 +0200

    mesh qui marche pas

--- testmesh.py
+++ testmesh.py
@@ -1,32 +1,35 @@
-import pymeshlab as pym
+import pymeshlab
 import numpy
+import pymeshlab.pmeshlab
+
+# m = pym.MeshSet()
+# m.load_new_mesh("C:\\Users\\inese\\Documents\\travail\\CI3\\ProjetP1\\projetp1\\airplane.obj")
+# m.save_current_mesh('aiplanee.obj')
 
 verts = numpy.array([
-    [-0.5, -0.5, -0.5],
-    [0.5, -0.5, -0.5],
-    [-0.5, 0.5, -0.5],
-    [0.5, 0.5, -0.5],
-    [-0.5, -0.5, 0.5],
-    [0.5, -0.5, 0.5],
-    [-0.5, 0.5, 0.5],
-    [0.5, 0.5, 0.5]])
+        [-0.5, -0.5, -0.5],
+        [0.5, -0.5, -0.5],
+        [-0.5, 0.5, -0.5],
+        [0.5, 0.5, -0.5],
+        [-0.5, -0.5, 0.5],
+        [0.5, -0.5, 0.5],
+        [-0.5, 0.5, 0.5],
+        [0.5, 0.5, 0.5]])
 
-# create a numpy 12x3 array of faces
-# every row represents a face (triangle in this case)
-# for every triangle, the index of the vertex
-# in the vertex array
 faces = numpy.array([
-    [2, 1, 0],
-    [1, 2, 3],
-    [4, 2, 0],
-    [2, 4, 6],
-    [1, 4, 0],
-    [4, 1, 5],
-    [6, 5, 7],
-    [5, 6, 4],
-    [3, 6, 7],
-    [6, 3, 2],
-    [5, 3, 7],
-    [3, 5, 1]])
+        [2, 1, 0],
+        [1, 2, 3],
+        [4, 2, 0],
+        [2, 4, 6],
+        [1, 4, 0],
+        [4, 1, 5],
+        [6, 5, 7],
+        [5, 6, 4],
+        [3, 6, 7],
+        [6, 3, 2],
+        [5, 3, 7],
+        [3, 5, 1]])
 
-m = pym.Mesh(verts, faces)
+pymeshlab
+m = pymeshlab.Mesh(verts, faces)
+print("fini")
