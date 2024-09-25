a53728f1db0fa18a1288b088cd9cb058948a2af7
pc-robot <ines.el.hadri@etu.imt-nord-europe.fr>
2024-09-25 18:10:24 +0200

    verif_ok

--- test_calc_rota.py
+++ test_calc_rota.py
@@ -2,15 +2,21 @@ import numpy as np
 from Robot import Robot
 from Transfo import matrice_to_pose, create_matrice
 
-def rotation(alpha, beta,gamma): 
+def rotation(gamma, beta,alpha): 
     #conversion si alpha, beta en degre et non radian 
     alpha=alpha*(np.pi/180)
     beta=beta*(np.pi/180)
     gamma=gamma*(np.pi/180)
-    Rx=np.asanyarray([[1,0,0],[0,np.cos(alpha),-np.sin(alpha)],[0,np.sin(alpha),np.cos(alpha)]])
-    Ry=np.asanyarray([[np.cos(beta),0,np.sin(beta)],[0,1,0],[-np.sin(beta),0,np.cos(beta)]])
-    Rz=np.asanyarray([[np.cos(gamma),-np.sin(gamma),0],[np.sin(gamma),np.cos(gamma),0],[0,0,1]])
-    return Rx @ Ry @ Rz
+    Rx=np.asanyarray([[1,            0,             0],
+                      [0,np.cos(alpha),-np.sin(alpha)],
+                      [0,np.sin(alpha), np.cos(alpha)]])
+    Ry=np.asanyarray([[np.cos(beta) ,0,np.sin(beta)],
+                      [0            ,1,           0],
+                      [-np.sin(beta),0,np.cos(beta)]])
+    Rz=np.asanyarray([[np.cos(gamma),-np.sin(gamma),0],
+                      [np.sin(gamma), np.cos(gamma),0],
+                      [0            , 0            ,1]])
+    return Rz @ Ry @ Rx
 
 def matrice_passage_normale(mat_rot,point):
     res=mat_rot.tolist()
@@ -32,7 +38,7 @@ if __name__=="__main__":
     robot.bouger(pos,0.5)
     for _ in range(18):
         alpha+=5
-        mat4x4=matrice_passage_normale(rotation(gamma,beta,alpha),point)
+        mat4x4=matrice_passage_normale(rotation(alpha,beta,gamma),point)
     #     # print(mat4x4)
         pos=matrice_to_pose(mat4x4)
         pos= robot.correction_pose(pos)
